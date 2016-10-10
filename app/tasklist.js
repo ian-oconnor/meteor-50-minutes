f42cbd4c36d59bff23b88704982239ab63b89efd
ian-oconnor <IO@hawaii.edu>
2016-10-09 22:46:45 -1000

    Task 2: Tasklist setup

---
+++ app/tasklist.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Ian O'Connor on 10/9/2016.
+ */
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
